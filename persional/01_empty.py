7bda0ebf740b7ca778ec047d10f355f52daf0573
vulcan_HP_22.04 <2912679550@qq.com>
2026-01-12 22:05:06 +0800

    0112-补充STL模型

---
+++ persional/01_empty.py
@@ -0,0 +1,39 @@
+import argparse
+from isaaclab.app import AppLauncher
+
+# 创建一个
+parser = argparse.ArgumentParser(description="Launch Isaac Lab with an empty scene.")
+AppLauncher.add_app_launcher_args(parser)
+args_cli = parser.parse_args()
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+
+from isaaclab.sim import SimulationCfg , SimulationContext
+
+
+def main():
+    """Main function."""
+
+    # Initialize the simulation context
+    sim_cfg = SimulationCfg(dt=0.01)
+    sim = SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([2.5, 2.5, 2.5], [0.0, 0.0, 0.0])
+
+    # Play the simulator
+    sim.reset()
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+
+    # Simulate physics
+    while simulation_app.is_running():
+        # perform step
+        sim.step()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
